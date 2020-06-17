043b20a3b1f4e0253493d8270263ee54347f8d5e
killerbee <rajunaga57@gmail.com>
2020-06-17 12:29:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Suspense } from 'react';
+import { Route,NavLink } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+import './App.css'
+// import Courses from './containers/Courses/Courses';
+// import Users from './containers/Users/Users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Courses=React.lazy(()=>import('./containers/Courses/Courses'));
+const Users=React.lazy(()=>import('./containers/Users/Users'));
+
+class App extends Component {
+  render () {
+    return (
+      <BrowserRouter>
+      <div className="App">
+      <section>
+        <nav>
+          <ul>
+            <li><NavLink exact to="/users">Users</NavLink></li>
+            <li><NavLink exact to="/Courses">Courses</NavLink></li>
+          </ul>
+        </nav>
+      </section>
+
+
+      <switch>
+      <Route path="/users" render={()=>(
+        <Suspense fallback={<div>Something went wrong</div>}>
+          <Users/>
+        </Suspense>
+      )}/>
+        
+      <Route path="/Courses" render={()=>(
+        <Suspense fallback={<div>Something went wrong.....!</div>}>
+          <Courses/>
+        </Suspense>
+      )}/>
+      </switch>
+        
+      </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
+
